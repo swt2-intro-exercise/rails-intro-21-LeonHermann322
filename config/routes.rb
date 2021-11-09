4a02bcce77b90650a5695132557cd5ec7870646d
LeonHermann322 <leon.hermann@student.hpi.uni-potsdam.de>
2021-11-09 16:14:42 +0100

    Solved issue #2

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :authors
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
