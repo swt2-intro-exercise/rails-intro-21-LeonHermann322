da24b105f14210b5383e66982811036452fbf565
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2021-11-13 14:08:15 +0100

    Failing test for #6

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'Index page', type: :feature do
+    it 'should render correctly' do
+        @alan = FactoryBot.create :author
+        visit authors_index
+        expect(page).to have_text 'Name'
+        expect(page).to have_text 'Homepage'
+        expect(page).to have_text "Alan Turing"
+        expect(page).to have_text "http://wikipedia.org/Alan_Turing"
+        expect(page).to have_link 'New', href: new_author_path
+    end
+end
