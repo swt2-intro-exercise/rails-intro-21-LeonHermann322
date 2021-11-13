0c7a8d28b9c2226bf3ded6f488e8f1078ad7adfe
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2021-11-13 15:30:26 +0100

    Solved issue #10

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Edit author page', type: :feature do
     it 'should update attributes of an author and no error should occur' do
         @author = FactoryBot.create :author
-        visit edit_author_url(@author)
+        visit edit_author_path(@author)
         page.fill_in 'author[first_name]', with: "Peter"
         find('input[type="submit"]').click
         @author.reload
